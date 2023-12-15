1e337de7384386c3ca5321e8fccd036a961526a5
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-15 19:51:10 +0300

    feat: added Drawer component

---
+++ src/components/Drawer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import DrawerHead from './DrawerHead.vue';
+</script>
+
+<template>
+	<div
+		class="fixed top-0 left-0 h-screen w-full bg-black z-10 opacity-70"
+	></div>
+	<DrawerHead />
+</template>
+
+<style scoped lang="scss"></style>
