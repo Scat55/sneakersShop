b16b68d2e9fedb9145e160496869a221a7cc738f
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-15 16:15:46 +0300

    feat: added Card component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 import Header from '@/components/Header.vue';
+import Card from '@/components/Card.vue';
 </script>
 
 <template>
 	<div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
 		<Header />
+
+		<div>
+			<Card />
+		</div>
 	</div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div
+		class="relative m-10 border border-slate-200 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+	>
+		<button>
+			<img
+				src="/like-2.svg"
+				alt="AddToFavorite"
+				class="absolute top-8 left-8"
+			/>
+		</button>
+		<img src="/sneakers/sneakers-1.jpg" alt="CardSneaker" />
+		<p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+
+		<div class="flex justify-between mt-5">
+			<div class="flex flex-col">
+				<span class="text-slate-400">Цена: </span>
+				<b>12 999 руб. </b>
+			</div>
+			<button>
+				<img src="/plus.svg" alt="AddToCard" />
+			</button>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss"></style>
